6b03ece23e0cfd8326ff0bec3cc3767c3eb79877
Louis Huang <louisopen@gmail.com>
2019-06-16 22:47:13 +0800

    update bash

--- bootapp.sh
+++ bootapp.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-sudo python3 /home/pi/WebStreaming.py
+#!/bin/sh 
+#This bash called by /home/pi/.config/autostart/autoboot.desktop
+jump_dir=/home/pi/PiCameraRecordingCSI
+#tr -d "\r" < oldname.sh > newname.sh   #if you can't cd to .... just do this command.
+cd $jump_dir
+pwd
+sudo python3 $jump_dir/WebStreaming.py
+
+exit 0
