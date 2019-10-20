99b5e742641ee557b3a141297de3cc001f7f0837
Louis Huang <louisopen@gmail.com>
2019-10-20 10:25:43 +0800

    update autorun

--- bootapp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh 
-#This bash called by /home/pi/.config/autostart/autoboot.desktop
-jump_dir=/home/pi/PiCameraRecordingCSI
-#tr -d "\r" < oldname.sh > newname.sh   #if you can't cd to .... just do this command.
-cd $jump_dir
-pwd
-sudo python3 $jump_dir/WebStreaming.py
-
-exit 0
